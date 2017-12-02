553fb5199fc43d2f217cffb26b036fb6a8beb4ea
JagjitKaur <sangherajagjit@gmail.com>
2017-12-02 02:50:22 -0500

    changes to add grid view

--- pwa-gallery/src/App.js
+++ pwa-gallery/src/App.js
@@ -1,41 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '',imageArray :[]};
 
     this.handleChange = this.handleChange.bind(this);
   }
 	handleChange(event) {
-		alert("val"+event.target.files);
 		var fileList = event.target.files;
-    let file = null;
+		let file = null;
 
-    for (let i = 0; i < fileList.length; i++) {
-      if (fileList[i].type.match(/^image\//)) {
-        file = fileList[i];
-        break;
+		for (let i = 0; i < fileList.length; i++) {
+			if (fileList[i].type.match(/^image\//)) {
+			file = fileList[i];
+			break;
       }
     }
 
     if (file !== null) {
-		this.setState({value: URL.createObjectURL(file)});
+		var existingArray = this.state.imageArray;
+		existingArray.push(URL.createObjectURL(file));
+		this.setState({imageArray: existingArray});
     }
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={this.state.value} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          take a picture
-        </p>
+        <header className="App-header" >
+          <h1 className="App-title">Enhance Me!</h1>
+		  <div className="App-gallery-container">
 		<input type="file" accept="image/*" capture="camera" onChange={this.handleChange}/>
+		</div>
+        </header>
+		<div id="photos">
+		{this.state.imageArray.map(function(name, index){
+                    return <img src={name} alt='test' />;
+                  })}
+		</div>
+        
       </div>
     );
   }
